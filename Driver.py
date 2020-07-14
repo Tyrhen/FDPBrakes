ed1900c0d0d2021fe35e67ef0d14971a5a78139b
Cody Boyd <CBoyd@fdpbrakes.com>
2020-07-14 09:12:35 -0400

    functionality to pass python variables into html

--- Driver.py
+++ Driver.py
@@ -1,7 +1,17 @@
 
+#Tools to implement variables into HTML File
+from jinja2 import Environment, FileSystemLoader
+file_loader = FileSystemLoader('.')
+env = Environment(loader = file_loader)
+template = env.get_template('base.html')
+
+template_vars = {"test": "testing" ,
+"Report_Standard":"SAE J2707 Report Examplllle"}
+html_out = template.render(template_vars)
+
 #Tools to covert HTML to PDF
 from weasyprint import HTML, CSS
-HTML('base.html').write_pdf('report.pdf')
+HTML(string = html_out).write_pdf('report.pdf', stylesheets=["Static/basic.css"])
 
 #Successful test of importing variables from another py file
 #can now separate the Datascience from the HTML formatting
@@ -9,12 +19,5 @@ import Datasci
 
 
 
-"""
-#Tools to implement variables into HTML File
-from jinja2 import Environment, FileSystemLoader
-file_loader = FileSystemLoader('templates')
-env = Environment(loader = file_loader)
-template = env.get_template('base.html')
-template_vars = {}
-html_out = template.render(template_vars)
-"""
+
+
