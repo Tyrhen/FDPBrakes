c6fa3b7f1fd0f0dc7f5f41f660776d46ff776f18
Cody Boyd <CBoyd@fdpbrakes.com>
2020-07-14 14:05:26 -0400

    attempts to make plots appear in html

--- Datasci.py
+++ Datasci.py
@@ -7,4 +7,4 @@ import seaborn as sns
 SAE_path = "Test Protocols w Tire Calculator.xlsx"
 SAE_data = pd.read_excel(SAE_path, sheet_name=4, index_col='Test Section')
 SAE_data.drop(["Description"], axis = 1, inplace = True) 
-xr = sns.lineplot(x = SAE_data['Initial Disc Brake Temperature (C)'], y = SAE_data['Final Speed (mph)'])
+xr = sns.regplot(x = SAE_data['Initial Disc Brake Temperature (C)'], y = SAE_data['Final Speed (mph)'])
--- Driver.py
+++ Driver.py
@@ -1,4 +1,5 @@
 
+import Datasci
 #Tools to implement variables into HTML File
 from jinja2 import Environment, FileSystemLoader
 file_loader = FileSystemLoader('.')
@@ -6,7 +7,9 @@ env = Environment(loader = file_loader)
 template = env.get_template('base.html')
 
 template_vars = {"test": "testing" ,
-"Report_Standard":"SAE J2707 Report Examplllle"}
+"Report_Standard":"SAE J2707 Report Example",
+"Make_And_Model": "2010 Ford F-150", "plot":
+Datasci.xr.to_html()}
 html_out = template.render(template_vars)
 
 #Tools to covert HTML to PDF
@@ -15,7 +18,7 @@ HTML(string = html_out).write_pdf('report.pdf', stylesheets=["Static/basic.css"]
 
 #Successful test of importing variables from another py file
 #can now separate the Datascience from the HTML formatting
-import Datasci
+
 
 
 
