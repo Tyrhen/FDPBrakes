5899cff88eedd08f56e138d4a0336ef2b00ea7ee
Ty Williams <Ty@MacBook-Pro.local>
2020-09-14 16:17:31 -0400

    minor editing

--- Driver.py
+++ Driver.py
@@ -2,22 +2,22 @@
 import Datasci
 import DataSci2
 
-if __name__ == "__main__":
-    """ creates PDF file from HTML """
-    def PDFconverter():
+""" creates PDF file from HTML """
+
+def PDFconverter():
         #tools for creating variable template environment converting HTML to PDF
-        from jinja2 import Environment, FileSystemLoader
-        from weasyprint import HTML, CSS
+    from jinja2 import Environment, FileSystemLoader
+    from weasyprint import HTML, CSS
           
         #loading the HTML file into an environment to pass variables to it
-        file_loader = FileSystemLoader('.')
-        env = Environment(loader = file_loader)
-        template = env.get_template('FDPBrakes/base.html')
+    file_loader = FileSystemLoader('.')
+    env = Environment(loader = file_loader)
+    template = env.get_template('FDPBrakes/base.html')
 
         #list of variables to pass 
-        template_vars = {
-            "Report_Standard":"Brake Dynamometer Test Report",
-            "Make_And_Model": "2019 Ford F-150",
+    template_vars = {
+            "Report_Standard":2,
+            "Make_And_Model": 1,
             "fig2_1": DataSci2.figure1,
             "fig2_2": DataSci2.figure2,
             "fig2_3": DataSci2.figure3,
@@ -35,10 +35,8 @@ if __name__ == "__main__":
            }
         
         #Rendering the variables in and then converting the HTML to PDF
-        html_out = template.render(template_vars)
-        HTML(string = html_out, base_url='.').write_pdf('report.pdf', stylesheets=["/Users/Ty/Desktop/FDP_brakes_proj_local/FDPBrakes/Static/basic.css"])
-   
-    PDFconverter()
+    html_out = template.render(template_vars)
+    HTML(string = html_out, base_url='.').write_pdf('report.pdf', stylesheets=["/Users/Ty/Desktop/FDP_brakes_proj_local/FDPBrakes/Static/basic.css"])
 
 
 
