388cbe5d28dddc601ff1d7d2ca9b954078c1fd50
Ty Williams <Ty@MacBook-Pro.local>
2020-10-01 13:40:00 -0400

    abbreviated test control flow for pdf creation

---
+++ quicktester.py
@@ -0,0 +1,28 @@
+import Datasci 
+import matplotlib
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns 
+def PDFconverter():
+
+
+    """
+	convert HTML into PDF dyanamically
+	"""
+	#tools for creating variable template environment converting HTML to PDF
+    from jinja2 import Environment, FileSystemLoader
+    from weasyprint import HTML, CSS
+
+    #loading the HTML file into an environment to pass variables to it
+    file_loader = FileSystemLoader('.')
+    env = Environment(loader = file_loader)
+    template = env.get_template('FDPBrakes/base.html')
+
+    #list of variables to pass
+    template_vars = {}   
+    #Rendering the variables in and then converting the HTML to PDF
+    html_out = template.render(template_vars)
+    HTML(string = html_out, base_url='.').write_pdf('testerreport.pdf', stylesheets=["/Users/Ty/Desktop/FDP_brakes_proj_local/FDPBrakes/Static/basic.css"])
+
+PDFconverter()
