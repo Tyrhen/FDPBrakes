233bb4d21b9fc97678907108ea18e2126a756098
Ty Williams <Ty@MacBook-Pro.local>
2020-11-21 00:32:48 -0500

    cleaner code

--- DataSci2.py
+++ DataSci2.py
@@ -1,51 +1,101 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns 
+import seaborn as sns
 from pandas.plotting import table
 
 
-data_path = "/Users/Ty/Desktop/FDP_brakes_proj_local/FDPBrakes/Datasets/FDP X 19 DATA.xlsx"
+data_path = (
+    "/Users/Ty/Desktop/FDP_brakes_proj_local/FDPBrakes/Datasets/FDP X 19 DATA.xlsx"
+)
 df1 = pd.read_excel(data_path, sheet_name="Three Sections")
-df1 = df1[df1["Final Temp"] > 0] 
+df1 = df1[df1["Final Temp"] > 0]
 df2 = pd.read_excel(data_path, sheet_name="Three Sections #2")
 
 sns.set_style("darkgrid")
 sns.set_palette("bright")
 
+
 def figure1():
-    figure1 = sns.lineplot(x= 'Stop Number', y = 'Friction Level', data=df1, hue = "Test Section",)
-    return figure1.figure.savefig("/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure1.svg")
+    figure1 = sns.lineplot(
+        x="Stop Number",
+        y="Friction Level",
+        data=df1,
+        hue="Test Section",
+    )
+    return figure1.figure.savefig(
+        "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure1.svg"
+    )
+
 
 def figure2():
-    figure2 = sns.lineplot(x="Stop Number", y="Friction Level", hue="Test Section", palette= "bright",  data= df2)
-    return figure2.figure.savefig("/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure2.svg")
+    figure2 = sns.lineplot(
+        x="Stop Number",
+        y="Friction Level",
+        hue="Test Section",
+        palette="bright",
+        data=df2,
+    )
+    return figure2.figure.savefig(
+        "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure2.svg"
+    )
+
 
 def figure3():
-    figure3 = sns.relplot(x="Friction Level", y="Final Temp", col="Test Section", palette= "bright", kind="line", data= df1)
-    return figure3.savefig("/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure3.svg")
+    figure3 = sns.relplot(
+        x="Friction Level",
+        y="Final Temp",
+        col="Test Section",
+        palette="bright",
+        kind="line",
+        data=df1,
+    )
+    return figure3.savefig(
+        "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure3.svg"
+    )
+
 
 def figure4():
-    figure4 = sns.relplot(x="Friction Level", y="Final Temp", col="Test Section", palette= "bright", kind="scatter", data= df2)
-    return figure4.savefig("/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure4.svg")
+    figure4 = sns.relplot(
+        x="Friction Level",
+        y="Final Temp",
+        col="Test Section",
+        palette="bright",
+        kind="scatter",
+        data=df2,
+    )
+    return figure4.savefig(
+        "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure4.svg"
+    )
+
 
 def figure5():
     figure5 = sns.catplot(x="Test Section", y="Friction Level", data=df1)
-    return figure5.savefig("/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure5.svg")
+    return figure5.savefig(
+        "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure5.svg"
+    )
+
 
 def figure6():
-    fig6 = sns.catplot(x="Test Section", y="Friction Level", kind = "violin", data=df2)
+    fig6 = sns.catplot(x="Test Section", y="Friction Level", kind="violin", data=df2)
     return fig6.savefig("/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure6.svg")
+
 
 def figure7():
     fig7 = sns.boxplot(x="Test Section", y="Friction Level", data=df1, whis=np.inf)
-    fig7 = sns.stripplot(x="Test Section", y="Friction Level", data=df1, color=".3") 
-    return fig7.figure.savefig("/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure7.svg")
+    fig7 = sns.stripplot(x="Test Section", y="Friction Level", data=df1, color=".3")
+    return fig7.figure.savefig(
+        "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure7.svg"
+    )
+
 
 def figure8():
-    y = df1.groupby(['Test Section']).mean()
-    fig8 = sns.barplot(x = 'Test Section', y = 'Friction Level', data = y.reset_index())
-    return fig8.figure.savefig("/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure8.svg")
+    y = df1.groupby(["Test Section"]).mean()
+    fig8 = sns.barplot(x="Test Section", y="Friction Level", data=y.reset_index())
+    return fig8.figure.savefig(
+        "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure8.svg"
+    )
+
 
 plt.clf()
 figure1()
@@ -65,9 +115,8 @@ plt.clf()
 figure8()
 
 
-
 figure1 = "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure1.svg"
-figure2 =  "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure2.svg"
+figure2 = "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure2.svg"
 figure3 = "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure3.svg"
 figure4 = "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure4.svg"
 figure5 = "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/figure5.svg"
--- Datasci.py
+++ Datasci.py
@@ -1,40 +1,49 @@
-#Tools for Data Science/Data Analysis
+# Tools for Data Science/Data Analysis
 
-import pandas as pd 
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns 
+import seaborn as sns
 
 
-#function to only pull in every 7th row of data (too many data points currently)
+# function to only pull in every 7th row of data (too many data points currently)
 def logic(index):
     if index % 7 == 0:
         return False
     return True
 
-#import data
-SAE_path = "/Users/Ty/Desktop/FDP_brakes_proj_local/FDPBrakes/Datasets/Test_Data_Filter.csv"
-SAE_data = pd.read_csv(SAE_path, skiprows= lambda x: logic(x) )
+
+# import data
+SAE_path = (
+    "/Users/Ty/Desktop/FDP_brakes_proj_local/FDPBrakes/Datasets/Test_Data_Filter.csv"
+)
+SAE_data = pd.read_csv(SAE_path, skiprows=lambda x: logic(x))
 
 
-#Break data into the test sections 
-Section_1 = SAE_data[(SAE_data['Next Test Section'] == 1)]
-Section_2 = SAE_data[SAE_data['Next Test Section'] == 2]
-Section_3 = SAE_data[SAE_data['Next Test Section'] == 3]
-
-
+# Break data into the test sections
+Section_1 = SAE_data[(SAE_data["Next Test Section"] == 1)]
+Section_2 = SAE_data[SAE_data["Next Test Section"] == 2]
+Section_3 = SAE_data[SAE_data["Next Test Section"] == 3]
 
 
 """Below are all the functions that produce graphs"""
-#general styling for plots
+# general styling for plots
 sns.set_style("whitegrid")
+
+
 def FIG1():
     sns.set_style("whitegrid")
     for item in SAE_data["Next Test Section"]:
         str(item)
-    fig1 = sns.FacetGrid(SAE_data, col = "Next Test Section")
-    fig1 = fig1.map(plt.scatter, "Mu", "Hydraulic Pressure", edgecolor = "w",)
+    fig1 = sns.FacetGrid(SAE_data, col="Next Test Section")
+    fig1 = fig1.map(
+        plt.scatter,
+        "Mu",
+        "Hydraulic Pressure",
+        edgecolor="w",
+    )
     return fig1.savefig("/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/fig1.svg")
+
 
 def FIG2():
     sns.set_style("darkgrid")
@@ -43,10 +52,16 @@ def FIG2():
     for item in SAE_data["Cycle_Count_Value"]:
         float(item)
 
-    fig2 = sns.FacetGrid(SAE_data, col = "Next Test Section")
-    fig2 = fig2.map(plt.scatter, "Mu", "Temp 3 Inboard (F)", edgecolor = "w",)
-    
+    fig2 = sns.FacetGrid(SAE_data, col="Next Test Section")
+    fig2 = fig2.map(
+        plt.scatter,
+        "Mu",
+        "Temp 3 Inboard (F)",
+        edgecolor="w",
+    )
+
     return fig2.savefig("/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/fig2.svg")
+
 
 def FIG5():
     sns.set_style("white")
@@ -54,9 +69,10 @@ def FIG5():
         str(item)
     for item in SAE_data["Cycle_Count_Value"]:
         float(item)
-    fig5 = sns.FacetGrid(SAE_data, col = "Next Test Section")
-    fig5 = fig5.map(plt.bar, "Cycle_Count_Value", "Mu", color = 'red', ecolor = 'black')
+    fig5 = sns.FacetGrid(SAE_data, col="Next Test Section")
+    fig5 = fig5.map(plt.bar, "Cycle_Count_Value", "Mu", color="red", ecolor="black")
     return fig5.savefig("/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/fig5.svg")
+
 
 def FIG4():
     sns.set_style("darkgrid")
@@ -64,22 +80,39 @@ def FIG4():
         str(item)
     for item in SAE_data["Cycle_Count_Value"]:
         float(item)
-    fig4 = sns.relplot(x="Mu", y="Temp 4 Outboard (F)", col="Next Test Section",  kind="scatter", data=SAE_data)
+    fig4 = sns.relplot(
+        x="Mu",
+        y="Temp 4 Outboard (F)",
+        col="Next Test Section",
+        kind="scatter",
+        data=SAE_data,
+    )
     return fig4.savefig("/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/fig4.svg")
 
+
 def FIG7():
-    fig7 = sns.lmplot(x="Mu", y="Temp 4 Outboard (F)", col="Next Test Section", hue="Next Test Section", data =SAE_data, fit_reg = True, col_wrap = 3, height = 3)
+    fig7 = sns.lmplot(
+        x="Mu",
+        y="Temp 4 Outboard (F)",
+        col="Next Test Section",
+        hue="Next Test Section",
+        data=SAE_data,
+        fit_reg=True,
+        col_wrap=3,
+        height=3,
+    )
     return fig7.savefig("/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/fig7.svg")
 
+
 def FIG3():
-    s2 = Section_2.drop('Next Test Section',axis =1)
+    s2 = Section_2.drop("Next Test Section", axis=1)
     corr = s2.corr()
     mask = np.zeros_like(corr)
     mask[np.triu_indices_from(mask)] = True
     with sns.axes_style("white"):
-     fig3, heat = plt.subplots(figsize=(7, 5))
-     heat = sns.heatmap(corr, mask=mask, vmax=.3, square=True)
-    
+        fig3, heat = plt.subplots(figsize=(7, 5))
+        heat = sns.heatmap(corr, mask=mask, vmax=0.3, square=True)
+
     fig3 = heat.get_figure()
     fig3.tight_layout()
     return fig3.savefig("/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/fig3.svg")
@@ -100,10 +133,9 @@ plt.clf()
 FIG7()
 
 
-
 """paths for figures"""
 fig1 = "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/fig1.svg"
-fig2 =  "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/fig2.svg"
+fig2 = "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/fig2.svg"
 fig3 = "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/fig3.svg"
 fig4 = "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/fig4.svg"
 fig5 = "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/fig5.svg"
@@ -111,8 +143,6 @@ fig6 = "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/fig6.svg"
 fig7 = "/Users/Ty/Desktop/FDP_brakes_proj_local/Figures/fig7.svg"
 
 """EXCEL to HTML"""
-#wb = pd.read_excel('D:\eclipse\test.xlsx') # This reads in your excel doc as a pandas DataFrame
+# wb = pd.read_excel('D:\eclipse\test.xlsx') # This reads in your excel doc as a pandas DataFrame
 
-#wb.to_html('C:\test.html') # Export the DataFrame (Excel doc) to an html file 
-
-
+# wb.to_html('C:\test.html') # Export the DataFrame (Excel doc) to an html file
